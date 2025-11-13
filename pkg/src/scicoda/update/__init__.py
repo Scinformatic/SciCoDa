b521cb2ff0610d0095fda139c7e527495fb8b0e0
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-11-13 06:28:07 +0100

    Update __init__.py

--- pkg/src/scicoda/update/__init__.py
+++ pkg/src/scicoda/update/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from scicoda.update import pdb
+from scicoda.update import pdb, pubchem
 from scicoda.data import _data_dir
 
 
@@ -8,4 +8,5 @@ def update_all(data_dir: Path | str | None = None):
     if data_dir is None:
         data_dir = _data_dir()
     pdb.update_all(data_dir=data_dir)
+    pubchem.update_all(data_dir=data_dir)
     return
