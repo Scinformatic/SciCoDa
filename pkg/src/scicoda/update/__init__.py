c9d5e88694beb898e24de6b57da84d0bf7e98797
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-28 08:26:43 +0100

    Update __init__.py

--- pkg/src/scicoda/update/__init__.py
+++ pkg/src/scicoda/update/__init__.py
@@ -1,13 +1,26 @@
-from pathlib import Path
+"""Update datasets in scicoda data package."""
 
-from scicoda.update import pdb, pubchem
+from pathlib import Path
+from typing import Any
+
+from . import atom, pdb
 from scicoda.data import _data_dir
 
 
-def update_all(data_dir: Path | str | None = None):
+def update_all(data_dir: Path | str | None = None) -> dict[str, Any]:
+    """Update all datasets in the scicoda data package.
+
+    Parameters
+    ----------
+    data_dir
+        Data directory of the package.
+        This must be left as `None`,
+        unless you want to generate data files
+        in a custom location, without affecting the package data.
+    """
     if data_dir is None:
         data_dir = _data_dir
     return {
+        "atom": atom.update_all(data_dir=data_dir),
         "pdb": pdb.update_all(data_dir=data_dir),
-        "pubchem": pubchem.update_all(data_dir=data_dir),
     }
