e758aacbdddf0b954bffc0778c13e0df65922b2a
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-28 23:16:29 +0100

    Update data.py

--- pkg/src/scicoda/data.py
+++ pkg/src/scicoda/data.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+import json
 from typing import TYPE_CHECKING, overload
 
 import pkgdata
-import pyserials
 import polars as pl
 
 from scicoda import exception
@@ -17,15 +17,90 @@ _data_dir = pkgdata.get_package_path_from_caller(top_level=True) / "data"
 _cache: dict[str, dict] = {}
 
 
-def get_data(category: str, name: str, cache: bool = True) -> dict | list:
-    return get(category, name, extension="yaml", cache=cache)["data"]
+@overload
+def get_file(
+    category: str,
+    name: str,
+    extension: Literal["json"] = "json",
+    *,
+    lazy: bool = False,
+    cache: bool = True,
+) -> dict | list: ...
+@overload
+def get_file(
+    category: str,
+    name: str,
+    extension: Literal["parquet"] = "parquet",
+    *,
+    lazy: bool = False,
+    cache: bool = True,
+) -> pl.DataFrame: ...
+@overload
+def get_file(
+    category: str,
+    name: str,
+    extension: Literal["parquet"] = "parquet",
+    *,
+    lazy: bool = True,
+    cache: bool = True,
+) -> pl.LazyFrame: ...
+def get_file(
+    category: str,
+    name: str,
+    extension: Literal["json", "parquet"],
+    *,
+    lazy: bool = False,
+    cache: bool = True,
+) -> dict | list | pl.DataFrame | pl.LazyFrame:
+    """Get a data file from the package data.
+
+    Parameters
+    ----------
+    category
+        Category of the data file.
+        This corresponds to the module name where the data can be accessed.
+    name
+        Name of the data file.
+        This corresponds to the function name that returns the data.
+    extension
+        File extension of the data file.
+    cache
+        Retain the data in memory after reading it
+        for faster access in subsequent calls.
+    lazy
+        If `extension` is "parquet",
+        return a `polars.LazyFrame` instead of a `polars.DataFrame`.
+
+    Returns
+    -------
+    The data file content.
+
+    Raises
+    -------
+    ScicodaFileNotFoundError
+        If the specified data file does not exist.
+    ScicodaInputError
+        If an unsupported file extension is specified.
+    """
+    if (cached := _cache.get(category, {}).get(name)) is not None:
+        return cached
+    filepath = get_filepath(category=category, name=name, extension=extension)
+    if extension == "json":
+        file = json.loads(filepath.read_text())
+    elif extension == "parquet":
+        file = pl.scan_parquet(filepath) if lazy else pl.read_parquet(filepath)
+    else:
+        raise exception.ScicodaInputError(
+            parameter="extension",
+            argument=extension,
+            message_detail="Unsupported file extension."
+        )
+    if cache:
+        _cache.setdefault(category, {})[name] = file
+    return file
 
 
-def get_schema(category: str, name: str, cache: bool = True) -> dict | list:
-    return get(category, name, extension="yaml", cache=cache)["schema"]
-
-
-def get_filepath(category: str, name: str, extension: str = "yaml") -> Path:
+def get_filepath(category: str, name: str, extension: Literal["json", "parquet"]) -> Path:
     """Get the absolute path to a data file.
 
     Parameters
@@ -57,91 +132,3 @@ def get_filepath(category: str, name: str, extension: str = "yaml") -> Path:
             filepath=filepath,
         )
     return filepath
-
-
-@overload
-def get(
-    category: str,
-    name: str,
-    extension: Literal["yaml"] = "yaml",
-    cache: bool = True,
-    lazy: bool = False
-) -> dict | list: ...
-@overload
-def get(
-    category: str,
-    name: str,
-    extension: Literal["parquet"] = "parquet",
-    cache: bool = True,
-    lazy: bool = False
-) -> pl.DataFrame: ...
-@overload
-def get(
-    category: str,
-    name: str,
-    extension: Literal["parquet"] = "parquet",
-    cache: bool = True,
-    lazy: bool = True
-) -> pl.LazyFrame: ...
-def get(
-    category: str,
-    name: str,
-    extension: Literal["yaml", "parquet"] = "yaml",
-    cache: bool = True,
-    lazy: bool = False
-) -> dict | list | pl.DataFrame | pl.LazyFrame:
-    """Get a data file from the package data.
-
-    Parameters
-    ----------
-    category
-        Category of the data file.
-        This corresponds to the module name where the data can be accessed.
-    name
-        Name of the data file.
-        This corresponds to the function name that returns the data.
-    extension
-        File extension of the data file.
-        Default is "yaml".
-    cache
-        Retain the data in memory after reading it
-        for faster access in subsequent calls.
-    lazy
-        If `extension` is "parquet",
-        return a `polars.LazyFrame` instead of a `polars.DataFrame`.
-
-    Returns
-    -------
-    The data file content.
-
-    Raises
-    -------
-    ScicodaFileNotFoundError
-        If the specified data file does not exist.
-    ScicodaInputError
-        If an unsupported file extension is specified.
-    """
-    if (cached := _cache.get(category, {}).get(name)) is not None:
-        return cached
-    filepath = get_filepath(category=category, name=name, extension=extension)
-    if extension == "yaml":
-        file = pyserials.read.yaml_from_file(filepath)
-        pyserials.validate.jsonschema(
-            data=file["data"],
-            schema=file["schema"],
-            fill_defaults=True,
-        )
-    elif extension == "parquet":
-        if lazy:
-            file = pl.scan_parquet(filepath)
-        else:
-            file = pl.read_parquet(filepath)
-    else:
-        raise exception.ScicodaInputError(
-            parameter="extension",
-            argument=extension,
-            message_detail="Unsupported file extension."
-        )
-    if cache:
-        _cache.setdefault(category, {})[name] = file
-    return file
