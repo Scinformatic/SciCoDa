9231087bd8f48c4bb129360ea498928c9da98d9c
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-28 08:26:53 +0100

    Update atom.py

--- pkg/src/scicoda/update/atom.py
+++ pkg/src/scicoda/update/atom.py
@@ -1,15 +1,30 @@
-"""Update atomic data files."""
+"""Update atomic datasets."""
 
 from pathlib import Path
 
 import polars as pl
 import dfhelp
 
-from scicoda.data import _data_dir, get_data
-from scicoda.create.atom import periodic_table as fetch_periodic_table
+from scicoda.data import _data_dir
+from scicoda.create import atom as create_atom
 
 
 def update_all(data_dir: Path | str | None = None) -> dict[str, dict[Path, pl.DataFrame]]:
+    """Update all datasets in the atom data category.
+
+    Parameters
+    ----------
+    data_dir
+        Data directory of the package.
+        This must be left as `None`,
+        unless you want to generate data files
+        in a custom location, without affecting the package data.
+
+    Returns
+    -------
+    A dictionary mapping dataset names to dictionaries,
+    which map file paths to their corresponding DataFrames.
+    """
     if data_dir is None:
         data_dir = _data_dir
     out = {
@@ -24,11 +39,12 @@ def periodic_table(
     *,
     url: str = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/periodictable/CSV"
 ) -> dict[Path, pl.DataFrame]:
-    """Download and store the periodic table from PubChem.
+    """Update and store the periodic table from PubChem.
 
-    This function retrieves the periodic table data from PubChem,
-    processes it into a polars DataFrame,
-    and saves it as a Parquet file.
+    Retrieve the periodic table data from PubChem,
+    process it into a polars DataFrame,
+    add additional data,
+    and save it as a Parquet file.
 
     Parameters
     ----------
@@ -43,14 +59,13 @@ def periodic_table(
 
     References
     ----------
+    - [PubChem Periodic Table](https://pubchem.ncbi.nlm.nih.gov/periodic-table/)
     - [IUPAC Cookbook](https://iupac.github.io/WFChemCookbook/datasources/pubchem_ptable.html)
     """
     if data_dir is None:
         data_dir = _data_dir
 
-    df = fetch_periodic_table(url=url)
-
-
+    df = create_atom.periodic_table(url=url)
 
     filepath = (Path(data_dir) / filepath).with_suffix(".parquet")
     dfhelp.write_parquet(df, filepath)
