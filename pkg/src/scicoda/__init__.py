8e2e5488434e4bfbc7744e29894524324c00c218
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-28 21:03:04 +0100

    Update __init__.py

--- pkg/src/scicoda/__init__.py
+++ pkg/src/scicoda/__init__.py
@@ -1,4 +1,32 @@
-"""SciCoDa: Scientific constants and data."""
+"""SciCoDa: Scientific constants and data.
+
+SciCoDa provides curated scientific datasets and constants for computational sciences,
+including computational chemistry, structural biology, and related fields.
+
+The package offers easy access to:
+- Protein Data Bank (PDB) Chemical Component Dictionary (CCD) data
+- Atomic and periodic table data from authoritative sources (PubChem, Blue Obelisk)
+- AutoDock atom types and properties
+
+All data is provided as Polars DataFrames for efficient manipulation and analysis.
+
+Modules
+-------
+atom
+    Atomic datasets including periodic table and AutoDock atom types.
+pdb
+    Protein Data Bank datasets including the Chemical Component Dictionary.
+
+Examples
+--------
+>>> import scicoda
+>>> # Get periodic table data
+>>> df = scicoda.atom.periodic_table()
+>>> # Get AutoDock atom types
+>>> df = scicoda.atom.autodock_atom_types()
+>>> # Get PDB chemical component data
+>>> df = scicoda.pdb.ccd()
+"""
 
 from scicoda import atom, pdb
 
