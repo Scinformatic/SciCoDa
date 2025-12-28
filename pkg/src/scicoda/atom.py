9162b49f0bbf4aca939d281bf878e8157aa441e6
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-28 08:35:36 +0100

    Update atom.py

--- pkg/src/scicoda/atom.py
+++ pkg/src/scicoda/atom.py
@@ -57,6 +57,9 @@ def autodock_atom_types(
 def periodic_table(cache: bool = True) -> pl.DataFrame:
     """Periodic table of chemical elements.
 
+    All data is sourced from PubChem,
+    unless otherwise noted.
+
     Parameters
     ----------
     cache
@@ -71,28 +74,48 @@ def periodic_table(cache: bool = True) -> pl.DataFrame:
     z : int
         Atomic number of the element.
     symbol : str
-        Chemical symbol of the element.
+        Chemical symbol of the element (capitalized),
+        e.g., 'H' for Hydrogen, 'He' for Helium.
     name : str
-        Name of the element.
+        Name of the element in lowercase,
+        e.g., 'hydrogen', 'helium'.
     period : int
-        Period number of the element in the periodic table.
+        Period number in the periodic table (1-7).
+    group : int
+        Group number in the periodic table (1-18),
+        or `null` for lanthanides and actinides
+        (La and Ac are considered part of group 3).
     block : str
         Group block of the element; one of:
-        'Alkali metal', 'Alkaline earth metal', 'Transition metal', 'Lanthanide', 'Actinide',
-        'Post-transition metal', 'Metalloid', 'Nonmetal', 'Halogen', 'Noble gas'.
-    e_config : str
-        Electron configuration of the element.
+        - 'actinide'
+        - 'alkali metal'
+        - 'alkaline earth metal'
+        - 'halogen'
+        - 'lanthanide'
+        - 'metalloid'
+        - 'noble gas'
+        - 'nonmetal'
+        - 'post-transition metal'
+        - 'transition metal'
+    econfig : str
+        Electron configuration of the element in standard notation,
+        e.g., '1s2', '[He]2s2 2p4'.
     mass : float
         Atomic mass of the element in unified atomic mass units (u).
-    r : float
+    vdwr : int
         Atomic (van der Waals) radius of the element in picometers (pm).
+    vdwr_bo : int
+        Atomic (van der Waals) radius of the element in picometers (pm),
+        from the [Blue Obelisk Data Repository](https://github.com/AAriam/bodr/blob/29ce17071c71b2d4d5ee81a2a28f0407331f1624/bodr/elements/elements.xml).
+        For elements beyond atomic number 109 (Ds to Og),
+        a default value of 2.00 Å is used to fill in missing data.
     ie : float
-        Ionization energy of the element in electronvolts (eV).
+        Ionization energy of the element in electronvolts (eV).§§§§
     ea : float
         Electron affinity of the element in electronvolts (eV).
     en_pauling : float
-        Electronegativity of the element.
-    ox_states : tuple[int] | None
+        Electronegativity of the element on the Pauling scale.
+    oxstates : list[int]
         Possible oxidation states of the element.
     state : str
         Standard state of the element at room temperature;
@@ -103,15 +126,21 @@ def periodic_table(cache: bool = True) -> pl.DataFrame:
         Boiling point of the element in kelvin (K).
     density : float
         Density of the element in grams per cubic centimeter (g/cm³).
-    cpk_color : str
-        CPK hex color code for the element.
-    year : str
-        Year the element was discovered.
+    color_cpk : str
+        CPK hex color code for the element, e.g., '#FF0000' for red.
+    year : int
+        Year the element was discovered,
+        or `null` if the element was known since ancient times.
+
+    Note that some elements, especially the synthetic ones,
+    may have missing values for some properties.
+    All missing values are represented as `null`.
 
     References
     ----------
     - [PubChem Periodic Table](https://pubchem.ncbi.nlm.nih.gov/periodic-table/)
     - [IUPAC Cookbook](https://iupac.github.io/WFChemCookbook/datasources/pubchem_ptable.html)
+    - [Blue Obelisk Data Repository](https://github.com/AAriam/bodr/blob/29ce17071c71b2d4d5ee81a2a28f0407331f1624/bodr/elements/elements.xml)
 
     See Also
     --------
